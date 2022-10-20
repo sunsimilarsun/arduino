f852633f379e3d55db26d7b8bac5d67a09065b9e
sunsimilarsun <sunsimilarsun@outlook.com>
2022-10-20 15:56:56 +0800

    测试

--- spi/src/main.cpp
+++ spi/src/main.cpp
@@ -2,13 +2,13 @@
  
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);
 }
